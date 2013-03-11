d3d9e0a0027e5799b29d20dd503d584d5df48c4b
Joseph Wynn <joseph@wildlyinaccurate.com>
2013-03-11 19:37:47 +0000

    Add views directory to watch:livereload config

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,6 +39,7 @@ module.exports = function (grunt) {
             livereload: {
                 files: [
                     '<%= yeoman.app %>/*.html',
+                    '<%= yeoman.app %>/views/*.html',
                     '{.tmp,<%= yeoman.app %>}/styles/{,*/}*.css',
                     '{.tmp,<%= yeoman.app %>}/scripts/{,*/}*.js',
                     '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,webp}'
